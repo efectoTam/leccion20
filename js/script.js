e5645a6bbbdc60052b6a179fe4be52ae525eba64
efectoTam <tam.luque@gmail.com>
2016-10-13 12:16:37 -0300

    Ejercicio sesion 20 listo

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ function callbackPromedio(a, b, c, callback){
 	var promedio=suma/3;
 	return callback(promedio);
 }
+
+function segundero(){
+    var t=1;
+    setInterval(function(){
+        document.getElementById("titulo").innerHTML=t++;
+    },1000,"JavaScript");
+}
+segundero();
